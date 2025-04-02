498cbf3d5dab409d478e95d70803b2311dbff83f
Nguyễn Đình Nhật <nhat.nd283@gmail.com>
2025-04-02 15:37:03 +0700

    Add  src/main/java/com/ndd/repositories/CategoryRepository.java

--- src/main/java/com/ndd/services/ProductService.java
+++ src/main/java/com/ndd/services/ProductService.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package com.ndd.repositories;
+package com.ndd.services;
 
 import com.ndd.pojos.Product;
 import java.util.List;
@@ -12,7 +12,7 @@ import java.util.Map;
  *
  * @author Apelion283
  */
-public interface ProductRepository {
+public interface ProductService {
 
     List<Product> getProducts(Map<String, String> params);
 
@@ -21,4 +21,5 @@ public interface ProductRepository {
     void deleteProduct(int id);
 
     Product addOrUpdate(Product p);
+
 }
