9ad2503134783891c75599a095e7b3c963533dd1
Nguyễn Đình Nhật <nhat.nd283@gmail.com>
2025-04-02 15:38:01 +0700

    Add implements of Product Service

---
+++ src/main/java/com/ndd/services/impl/ProductServiceImpl.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ndd.services.impl;
+
+import com.ndd.pojos.Product;
+import com.ndd.repositories.ProductRepository;
+import com.ndd.services.ProductService;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Apelion283
+ */
+@Service
+public class ProductServiceImpl implements ProductService{
+    
+    @Autowired
+    ProductRepository prodRepo;
+
+    @Override
+    public List<Product> getProducts(Map<String, String> params) {
+        return this.prodRepo.getProducts(params);
+    }
+
+    @Override
+    public Product getProductById(int id) {
+        return this.prodRepo.getProductById(id);
+    }
+
+    @Override
+    public void deleteProduct(int id) {
+        this.prodRepo.deleteProduct(id);
+    }
+
+    @Override
+    public Product addOrUpdate(Product p) {
+        return this.prodRepo.addOrUpdate(p);
+    }
+    
+}
