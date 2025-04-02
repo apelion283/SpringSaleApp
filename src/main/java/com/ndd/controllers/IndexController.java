a0f5a01fd1d243f00f8fc46e039a625ea85b9920
Nguyễn Đình Nhật <nhat.nd283@gmail.com>
2025-04-02 15:21:45 +0700

    Handle common response with Model Attribute of categories

--- src/main/java/com/ndd/controllers/IndexController.java
+++ src/main/java/com/ndd/controllers/IndexController.java
@@ -5,24 +5,38 @@
 package com.ndd.controllers;
 
 import com.ndd.services.CategoryService;
+import com.ndd.services.ProductService;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
  * @author Apelion283
  */
 @Controller
+@ControllerAdvice
 public class IndexController {
 
     @Autowired
     private CategoryService catService;
+    
+    @Autowired
+    private ProductService prodsService;
+    
+    @ModelAttribute
+    public void commonRespond(Model model){
+        model.addAttribute("categories", this.catService.getCats());
+    }
 
     @RequestMapping("/")
-    public String index(Model model) {
-        model.addAttribute("categories", this.catService.getCats());
+    public String index(Model model, @RequestParam Map<String, String> params) {
+        model.addAttribute("products", this.prodsService.getProducts(params));
         return "index";
     }
 }
