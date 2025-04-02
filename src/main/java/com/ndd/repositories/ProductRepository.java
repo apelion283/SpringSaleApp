bcc03d7900307f4af701afc0723c90eb9096327d
Nguyễn Đình Nhật <nhat.nd283@gmail.com>
2025-04-02 16:02:07 +0700

    Add Product Repository Interface

---
+++ src/main/java/com/ndd/repositories/ProductRepository.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.ndd.repositories;
+
+import com.ndd.pojos.Product;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author Apelion283
+ */
+public interface ProductRepository {
+    List<Product> getProducts(Map<String, String> params);
+
+    Product getProductById(int id);
+
+    Product addOrUpdate(Product p);
+
+    void deleteProduct(int id);
+}
